a6cb42d673044530f28cad169b9259d8f6dc2eb3
Ramachari787 <rampushpagiri@gmail.com>
2019-08-29 18:25:19 +0530

    Update connections.tf

--- connections.tf
+++ connections.tf
@@ -1,19 +1,9 @@
-provider "google" {
-  credentials = "${file("../account.json")}"
-  project     = "thrashingcorecode"
-  region      = "us-west1"
-}
+
 
 provider "aws" {
-  region = "us-west-2"
+  region = "US Eest"
 }
 
-provider "azurerm" {
-  subscription_id = "${var.subscription_id}"
-  client_id       = "${var.client_id}"
-  client_secret   = "${var.client_secret}"
-  tenant_id       = "${var.tenant_id}"
-}
 
 variable subscription_id {}
 variable client_id {}
